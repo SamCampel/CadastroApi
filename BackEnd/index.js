1927ced7aa8e07de9b815345b907c8defb5e611a
Samuel Campelo <>
2025-03-28 23:10:43 -0300

    iniciando index.js backend

---
+++ BackEnd/index.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+
+const app = express();
+
+
+app.get('/users', (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+app.listen(5768)
