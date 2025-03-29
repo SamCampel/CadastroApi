8a6368bfc29a7dbc5d2c72527c20a070c27bc903
Samuel Campelo <>
2025-03-29 16:11:00 -0300

    backend final

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,6 +7,39 @@ app.use(express.json())
 
 const users = []
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        await prisma.user.delete({
+            where: {
+                id: req.params.id
+            }
+        }) 
+    } catch (error) {
+        console.log("Erro em deletar usuário.")
+    }
+    
+})
+app.put('/users/:id', async (req, res) => {
+    try {
+        await prisma.user.update({
+
+            where: {
+                id: req.params.id
+            },
+            data: {
+                email: req.body.email,
+                name: req.body.name,
+                age: req.body.age
+            }
+
+        })
+
+        res.status(201).json(re.body)
+    } catch (error) {
+        console.log("Erro em atualizar usuário.")
+    }
+})
+
 app.post('/users', async (req, res) => {
     try {
         await prisma.user.create({
@@ -20,17 +53,26 @@ app.post('/users', async (req, res) => {
 
         res.status(201).json(re.body)
     } catch (error) {
-
+        console.log("Erro em criar usuário.")
     }
 })
 
-app.get('/users', (req, res) => {
-    try {
+app.get('/users', async (req, res) => {
 
-        res.status(200).req.json(users)
-    } catch (error) {
-
+    let users = []
+    if(req.query){
+        await prisma.user.findMany({
+            where:{
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age
+            }
+        })
+    } else {
+        await prisma.user.findMany()
     }
+
+    res.status(200).req.json(users)
 })
 
 app.listen(5678, () => {
